302a234dd6a39f97f1f9bf8645a694037d9936b1
Rhon <51997455+EdiciusRift@users.noreply.github.com>
2020-04-22 15:57:53 +0800

    Update functions.js

--- public/js/functions.js
+++ public/js/functions.js
@@ -287,27 +287,27 @@ $(function(){
     });
 
     $(document).on("click", "#viewReview", function(){
-        var href = $(this).attr('href') + window.location.href.substring(36, 50);
+        var href = $(this).attr('href') + window.location.href.substring(34, 49);
         $(this).attr('href', href);
     });
 
     $(document).on("click", "#review", function(){
-        var href = $(this).attr('href') + window.location.href.substring(36, 50);
+        var href = $(this).attr('href') + window.location.href.substring(34, 49);
         $(this).attr('href', href);
     });
 
     $(document).on("click", "#editReview", function(){
-        var href = $(this).attr('href') + window.location.href.substring(36, 50);
+        var href = $(this).attr('href') + window.location.href.substring(34, 49);
         $(this).attr('href', href);
     });
 
     $(document).on("click", "#it_viewDownload", function(){
-        var href = $(this).attr('href') + window.location.href.substring(36);
+        var href = $(this).attr('href') + window.location.href.substring(34);
         $(this).attr('href', href);
     });
 
     $(document).on("click", "#viewEdit", function(){
-        var href = $(this).attr('href') + window.location.href.substring(36);
+        var href = $(this).attr('href') + window.location.href.substring(34);
         $(this).attr('href', href);
     });
 
